c01393b325e874eac847ff3a4541b517a7178ca3
Rylan Sturm <rylan.sturm@gmail.com>
2024-06-06 23:39:07 -0600

    added a frequency dictionary. Still needs some tuning

--- wordle.py
+++ wordle.py
@@ -1,11 +1,14 @@
 WORDS = []
 TEMP_WORDS = []
+FREQ_DICT = {} #{c:0 for c in 'abcdefghijklmnopqrstuvwxyz'}
+WORD_SCORES = {}
 
 def load_file(f = 'words.txt'):
     global WORDS
     with open(f) as infile:
         for word in infile:
             WORDS.append(word.strip())
+    make_word_scores()
 
 def remove_char(c):
     global WORDS, TEMP_WORDS
@@ -14,6 +17,7 @@ def remove_char(c):
             TEMP_WORDS.append(word)
     WORDS = TEMP_WORDS.copy()
     TEMP_WORDS = []
+    make_word_scores()
 
 def set_char(c, i):
     global WORDS, TEMP_WORDS
@@ -22,6 +26,7 @@ def set_char(c, i):
             TEMP_WORDS.append(word)
     WORDS = TEMP_WORDS.copy()
     TEMP_WORDS = []
+    make_word_scores()
 
 def set_yellow(c, i):
     global WORDS, TEMP_WORDS
@@ -30,6 +35,7 @@ def set_yellow(c, i):
             TEMP_WORDS.append(word)
     WORDS = TEMP_WORDS.copy()
     TEMP_WORDS = []
+    make_word_scores()
 
 def set_chars(s, **kwargs):
     yellow_pos = []
@@ -42,9 +48,40 @@ def set_chars(s, **kwargs):
             pass
         else:
             set_char(s[i], i+1)
+    make_word_scores()
 
-def pwords():
+def make_freq_dict():
+    global FREQ_DICT
     global WORDS
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        FREQ_DICT[c] = 0
+    for word in WORDS:
+        for c in word:
+            FREQ_DICT[c] += 1
+
+def make_word_scores():
+    global WORD_SCORES
+    global FREQ_DICT
+    global WORDS
+    make_freq_dict()
+    for word in WORDS:
+        score = 0
+        WORD_SCORES[word] = score
+        for c in word:
+            score += FREQ_DICT[c]
+        WORD_SCORES[word] = score
+
+def pwords(sort_by_freq = False, qty=None):
+    global WORDS, WORD_SCORES
+    if sort_by_freq:
+        make_word_scores()
+        i = 0
+        for k, v in sorted(WORD_SCORES.items(), key=lambda p:p[1], reverse=True):
+            print(k, v)
+            i += 1
+            if qty and i >= qty:
+                return
+        return
     for word in WORDS:
         print(word)
 
